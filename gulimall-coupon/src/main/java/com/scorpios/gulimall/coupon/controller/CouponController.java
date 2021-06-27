dad2a7b30e84c0ded1bf376bef0a973a105c025b
scorpios <1435513775@qq.com>
2021-06-27 19:43:54 +0800

    nacos配置中心

--- gulimall-coupon/src/main/java/com/scorpios/gulimall/coupon/controller/CouponController.java
+++ gulimall-coupon/src/main/java/com/scorpios/gulimall/coupon/controller/CouponController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,11 +26,24 @@ import com.scorpios.gulimall.common.utils.R;
  * @email 1435513775@qq.com
  * @date 2021-06-26 19:54:18
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
+
     @Autowired
     private CouponService couponService;
+
+    @Value("${coupon.user.name}")
+    private String name;
+
+    @Value("${coupon.user.age}")
+    private Integer age;
+
+    @RequestMapping("/test")
+    public R test(){
+        return R.ok().put("name",name).put("age",age);
+    }
 
     @RequestMapping("/member/list")
     public R memberCoupons(){
